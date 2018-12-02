b5d9c8b9218b338c299b39cf4179978e65b70bdb
decantr <theeasterlywood@gmail.com>
2018-12-02 13:28:26 +0000

    lab3 4.4.7

--- third/src/weatherServlet.java
+++ third/src/weatherServlet.java
@@ -13,7 +13,19 @@ protected void doPost( HttpServletRequest request , HttpServletResponse response
 }
 
 protected void doGet( HttpServletRequest request , HttpServletResponse response ) throws ServletException, IOException {
-	String place = request.getParameter( "place" );
+	String place = "";
+	char unit = 'C';
+	double temp;
+	String p = request.getParameter( "place" );
+	String u = request.getParameter( "unit" );
+
+//	check if p was not null
+	if ( p != null )
+		place = p;
+//	check if u was not null and if its wasn't make sure F was passed
+	if ( u != null )
+		if ( u.toUpperCase().charAt( 0 ) == 'F' )
+			unit = 'F';
 
 	response.setContentType( " text/plain" );
 
@@ -21,11 +33,15 @@ protected void doGet( HttpServletRequest request , HttpServletResponse response 
 
 	WeatherInfo wi = wd.find( place );
 
+// if weather info fails return 403
 	if ( wi == null ) {
 		response.sendError( 403 );
 		return;
 	}
 
-	response.getWriter().println( place + ": " + wi.condition + " " + wi.temperature + wi.unit );
+	// if unit has been passed as Fahrenheit then convert otherwise fallback to Celsius
+	temp = unit == 'F' ? Math.round(( wi.temperature * 1.8 ) + 32) : wi.temperature;
+
+	response.getWriter().println( place + ": " + wi.condition + " " + temp + unit );
 }
 }
